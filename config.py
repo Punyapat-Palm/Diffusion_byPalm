be39fff938388bcb9fe0b8cebaa6def6d7aa992f
Palm @_@ <75152483+Punyapat-Palm@users.noreply.github.com>
2024-07-19 12:30:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 config = {
     "diffusion_steps": 1000,
-    "dataset": r"Dataset\train_images",
+    "dataset": r"Dataset/train_images",
     "max_epoch": 10,
     "continue_train": False,
     "version": 0,
