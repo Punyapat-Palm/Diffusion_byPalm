9ebbd9f9a47ba5f9cce24a7135b72f12cdf0854b
Palm @_@ <75152483+Punyapat-Palm@users.noreply.github.com>
2024-10-25 14:08:27 +0700

    Add files via upload

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
-import numpy as np
 import math
+import numpy as np
 from modules import *
 
 class DiffusionModel(nn.Module):
@@ -11,37 +11,40 @@ class DiffusionModel(nn.Module):
         self.t_range = t_range
         self.in_size = in_size
         self.device = device
-        self.unet = Unet(dim = 64, dim_mults = (1, 1, 2, 2, 3, 4), channels=img_depth)
-        self.betas = self.get_named_beta_schedule(t_range)
+        self.unet = Unet(dim = 64, dim_mults = (1, 2, 4, 8), channels=img_depth)
+        self.betas = self.calculate_betas(self.t_range)
         self.alphas = 1 - self.betas
         self.alpha_bars = torch.cumprod(self.alphas, dim=0)
 
     def forward(self, x, t):
         return self.unet(x, t)
-
-    def get_named_beta_schedule(self, num_diffusion_timesteps):
-        return torch.tensor(
-            self.betas_for_alpha_bar(
-                num_diffusion_timesteps,
-                lambda t: math.cos((t + 0.008) / 1.008 * math.pi / 2) ** 2
-            ), 
-            device=self.device
-        )
-
-    def betas_for_alpha_bar(self, num_diffusion_timesteps, alpha_bar, max_beta=0.999):
+    
+    def calculate_betas(self, t_range):
         betas = []
-        for i in range(num_diffusion_timesteps):
-            t1 = i / num_diffusion_timesteps
-            t2 = (i + 1) / num_diffusion_timesteps
-            betas.append(min(1 - alpha_bar(t2) / alpha_bar(t1), max_beta))
-        return np.array(betas)
+        for i in range(t_range):
+            t1 = i / t_range
+            t2 = (i + 1) / t_range
+            alpha_bar_t1 = math.cos((t1 + 0.008) / 1.008 * math.pi / 2) ** 2
+            alpha_bar_t2 = math.cos((t2 + 0.008) / 1.008 * math.pi / 2) ** 2
+            beta = min(1 - alpha_bar_t2 / alpha_bar_t1, 0.999)
+            betas.append(beta)
+            
+        return torch.tensor(betas, device=self.device)
+
+    def beta(self, t):
+        return self.betas[t]
+
+    def alpha(self, t):
+        return 1 - self.beta(t)
+
+    def alpha_bar(self, t):
+        return self.alpha_bars[t]
 
     def get_loss(self, batch):
         ts = torch.randint(0, self.t_range, [batch.shape[0]], device=self.device)
         epsilons = torch.randn(batch.shape, device=self.device)
         a_hat = self.alpha_bars[ts].unsqueeze(1).unsqueeze(2).unsqueeze(3)
         noise_imgs = (a_hat.sqrt() * batch) + ((1 - a_hat).sqrt() * epsilons)
-        noise_imgs = noise_imgs.float()
         e_hat = self.forward(noise_imgs, ts)
         loss = nn.functional.mse_loss(
             e_hat.view(-1, self.in_size), epsilons.view(-1, self.in_size)
@@ -58,4 +61,3 @@ class DiffusionModel(nn.Module):
             post_sigma = math.sqrt(self.beta(t)) * z
             x = pre_scale * (x - e_scale * e_hat) + post_sigma
             return x
-        
